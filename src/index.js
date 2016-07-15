c1390fcae368e50651cc9118ebc15bf68bc88ed1
charles <charleslavong@gmail.com>
2016-07-15 16:01:04 +0200

    Using a provider to allow implicit passing of state from the provider to it's child components via redux.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 //import { Router, browserHistory } from 'react-router';
 //import routes from './routes';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './styles/styles.scss';
 //import { syncHistoryWithStore } from 'react-router-redux';
 
 import HomeContainer from './containers/HomePageContainer';
@@ -24,6 +24,8 @@ const store = configureStore();
 );*/
 
 render(
-  <HomeContainer store={store} />,
+  <Provider store={store} >
+    <HomeContainer />
+  </Provider>,
   document.getElementById('app')
 );
