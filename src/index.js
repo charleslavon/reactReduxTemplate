1b43a35702029b782adcfd68cc6aeeb65c95662f
charles <charleslavong@gmail.com>
2016-07-15 16:22:14 +0200

    Using Provider with Router and implicitly passing state to the components

--- src/index.js
+++ src/index.js
@@ -3,29 +3,22 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-//import { Router, browserHistory } from 'react-router';
-//import routes from './routes';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss';
-//import { syncHistoryWithStore } from 'react-router-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import HomeContainer from './containers/HomePageContainer';
 
 const store = configureStore();
 
 // Create an enhanced history that syncs navigation events with the store
-//const history = syncHistoryWithStore(browserHistory, store);
-
-/*render(
-  <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>, document.getElementById('app')
-);*/
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <Provider store={store} >
-    <HomeContainer />
-  </Provider>,
-  document.getElementById('app')
+  <Provider store={store}>
+    <Router history={history} routes={routes}/>
+  </Provider>, document.getElementById('app')
 );
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
+import HomeContainer from './containers/HomePageContainer';
 import AboutPage from './components/AboutPage.js';
 import NotFoundPage from './components/NotFoundPage.js';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={HomePage}/>
+  <Route path="/" component={App} >
+    <IndexRoute component={HomeContainer}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
