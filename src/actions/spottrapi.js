8f65f935927cc934841c21cb501684650e5d0f0c
charles <charleslavong@gmail.com>
2016-12-12 20:13:19 +0100

    Removing some unused code.

--- src/actions/spottrapi.js
+++ src/actions/spottrapi.js
@@ -8,6 +8,8 @@ axios.defaults.headers.post['Content-Type'] = "application/graphql";
 /*curl -XPOST -H "Content-Type:application/graphql" -d 'mutation {addAthlete(name: "antonio", email:"antonio@spottr.fit") {id, name, email, latitude, longitude} }' http://localhost:4000/spottrql
 */
 
+
+
 /* curl -XPOST -H "Content-Type:application/graphql" -d 'query {workouts {id, description, author {id, name, email}, likes, comments {id, author {name}, comment}, attendees {id, name}}}' http://clg.local.com:4000/spottrql
 */
 
--- src/reducers/wodReducer.js
+++ src/reducers/wodReducer.js
@@ -1,9 +1,8 @@
 import * as types from '../constants/actionTypes';
-import InitialState from './initialState';
 import uuid from 'uuid';
 
 //seperating concerns with the use of multiple specialized reducers
-const reducers = (state = InitialState.wods, action ) => {
+const reducers = (state = [], action ) => {
   switch(action.type) {
     case types.ADD_WOD:
       return [
@@ -83,27 +82,5 @@ const attendees = (wod, action) => {
       return wod;
   }
 };
-
-
-//http request example
-const googlePOI = (state) => {
-
-    let resp = getRequest("https://maps.googleapis.com/maps/api/place/search/json?location=41.3851,2.1734&radius=100&key=AIzaSyBg8HI6sH1Rxyhn1Mno_hhgDawuF1KAfq0");
-    return Object.assign({}, state, JSON.parse(resp).results);
-
-
-    function getRequest(url){
-      let httpRequest = new XMLHttpRequest();
-
-      httpRequest.onreadystatechange = () => {
-        if(httpRequest.readyState === XMLHttpRequest.DONE) {
-            return httpRequest.responseText;
-        }
-      };
-      httpRequest.open('GET', url);
-      httpRequest.send();
-    }
-};
-
 
 export default reducers;
