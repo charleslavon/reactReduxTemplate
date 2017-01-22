a9810ede0022f6746e94988c6cae9bf487c78232
charles <charleslavong@gmail.com>
2017-01-22 10:04:50 +0100

    Adding dimensions to support Galaxy s6 browser

--- src/components/Startup.js
+++ src/components/Startup.js
@@ -11,15 +11,14 @@ const Startup = (props) => {
         <section className="banner">
           <img className="logo-large" src={logo} alt="Spottr large logo"/>
         </section>
-        <section className="banner-flag">
-        </section>
+        <section className="banner-flag"/>
         <section className="tagline">
           <p className="highlight-text">
             Fitness with a friend
           </p>
         </section>
         <section className="login">
-          <input type="email" size="22" placeholder="email address"/>
+          <input type="email" placeholder="email address"/>
           <img src={go} alt="login"/>
         </section>
       </div>
@@ -28,7 +27,6 @@ const Startup = (props) => {
 };
 
 Startup.propTypes = {
-  props: PropTypes.element.required
 };
 
 
