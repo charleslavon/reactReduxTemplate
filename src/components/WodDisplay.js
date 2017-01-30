8cd69eb03728a4b0cadd2e6aa18d3d6380b1796a
charles <charleslavong@gmail.com>
2017-01-30 18:18:23 +0100

    Setting the workout display framing rules for all supported devices.

--- src/components/WodDisplay.js
+++ src/components/WodDisplay.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import '../styles/workouts.scss';
 
-
 const WodDisplay = props => {
 
     return (
-      <article>
-          <section className="header">
-              {props.author}
-              <span>hace 4 horas</span>
-          </section>
-          <section className="description">
-              {props.description}
-          </section>
-          <section className="carousel">
-              {/* fotos and videos here */}
-          </section>
-          <section className="footer">
-              <div>
-                  <img src="" alt=""/>
-              </div>
-              <div>
-                  <img src="" alt=""/>
-              </div>
-              <div>
-                  <img src="" alt=""/>
-              </div>
-          </section>
+        <article>
+            <section className="header">
+                {props.author}
+                <span>hace 4 horas</span>
+            </section>
+            <section className="description">
+                {props.description}
+            </section>
+            <section className="carousel">
+                {/* fotos and videos here */}
+            </section>
+            <section className="footer">
+                <div>
+                    <iron-icon icon="social:person-outline"/>
+                </div>
+                <div>
+                    <iron-icon icon="communication:chat-bubble-outline"/>
+                </div>
+                <div>
+                    <iron-icon icon="social:share"/>
+                </div>
+            </section>
         </article>
-  );
-};
+    );
 
+};
 
 WodDisplay.propTypes = {
   id: React.PropTypes.string,
