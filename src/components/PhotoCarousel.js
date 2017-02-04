629cc7c55330fe7ac9fdb07c1f2d27b79ccb418c
charles <charleslavong@gmail.com>
2017-02-04 09:18:30 +0100

    Dynamically adjusting the dimensions of the foto panes based on the number of images to be displayed

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -11,25 +11,25 @@ const Photo = props => {
             <div className="carousel">
 
                 { props.images.length === 2 &&
-                    <ul className="panes">
-                        <li>
+                    <ul className="panes" style={{"width": "300%"}}>
+                        <li style={{"width": "33%"}}>
                             <img src={user1_1}/>
                         </li>
-                        <li>
+                        <li style={{"width": "33%"}}>
                             <img src={user1_2}/>
                         </li>
-                        <li className="imageUpload" onClick={() => {
+                        <li style={{"width": "33%"}} className="imageUpload" onClick={() => {
                             document.querySelector("input[type=file]").click();
                         }}>
                             <input type="file" accept="image/*"/>
                         </li>
                     </ul>
                 }{ props.images.length === 3 &&
-                        <ul className="panes">
-                            <li>
+                        <ul className="panes" style={{"width": "200%"}}>
+                            <li style={{"width": "50%"}}>
                                 <img src={beach}/>
                             </li>
-                            <li className="imageUpload" onClick={() => {
+                            <li style={{"width": "50%"}} className="imageUpload" onClick={() => {
                                 document.querySelector("input[type=file]").click();
                             }}>
                                 <input type="file" accept="image/*"/>
@@ -37,8 +37,8 @@ const Photo = props => {
                         </ul>
                     }
                 { props.images.length === 0 &&
-                <ul className="panes">
-                    <li className="imageUpload" onClick={() => {
+                <ul className="panes" style={{"width": "100%"}}>
+                    <li style={{"width": "100%"}} className="imageUpload" onClick={() => {
                         document.querySelector("input[type=file]").click();
                     }}>
                         <input type="file" accept="image/*"/>
