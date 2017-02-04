5f1810c3033274d8218b3755f101617e70cb8e3d
charles <charleslavong@gmail.com>
2017-02-04 10:14:08 +0100

    Simplifying the app frame by getting rid of the bottom frame buttons and moving critical buttons to the top header.

--- src/components/AppFrame.js
+++ src/components/AppFrame.js
@@ -1,6 +1,5 @@
 import React, {PropTypes} from 'react';
 import spottr_logo from '../images/spottr_logo.png';
-import app_logo from '../images/spottr_app_logo.png';
 import plus from '../images/plus_freepix.png';
 import profile from '../images/profile_freepix.png';
 import inbox from '../images/inbox_freepix.png';
@@ -13,17 +12,14 @@ const AppFrame = props => {
                   rel="import" href="/bower_components/paper-badge/paper-badge.html"/>
             <section className="header">
                 <img className="logo" src={spottr_logo} alt="spottr logo"/>
+                <img className="plus" src={plus} alt="add a workout event"/>
+                <img className="profile" src={profile} alt="your profile"/>
                 <img className="inbox" id="inbox" src={inbox} alt="inbox"/>
                 <paper-badge class="inbox" for="inbox" label="4"></paper-badge>
             </section>
             <div className="main-view">
                 {props.children}
             </div>
-            <footer>
-                <img className="app-logo" src={app_logo} alt="main view"/>
-                <img className="plus" src={plus} alt="add a workout"/>
-                <img className="profile" src={profile} alt="your profile"/>
-            </footer>
         </div>
     );
 };
