50b8d0f14d25d8717c6317502d44b31ccff6e000
charles <charleslavong@gmail.com>
2017-02-06 20:54:07 +0100

    Fixing some JSlint warnings.

--- src/components/WodDisplay.js
+++ src/components/WodDisplay.js
@@ -15,17 +15,16 @@ const WodDisplay = props => {
             </section>
             <section className="carousel">
                 {/* fotos and videos here */}
-                <PhotoCarousel images={props.images}></PhotoCarousel>
+                <PhotoCarousel images={props.images}/>
             </section>
             <section className="footer">
                 <div className="attendees">
                     <iron-icon id="attendeesIconId" icon="social:person-outline"/>
-                    <paper-badge class="attendees" for="attendeesIconId" label="1"></paper-badge>
+                    <paper-badge class="attendees" for="attendeesIconId" label="1"/>
                 </div>
                 <div className="chat">
                     <iron-icon id="chatIconId" icon="communication:chat-bubble-outline"/>
-                    <paper-badge icon="social:mood" class="chat" for="chatIconId" label="happy">
-                    </paper-badge>
+                    <paper-badge icon="social:mood" class="chat" for="chatIconId" label="happy"/>
                 </div>
                 <div className="share">
                     <iron-icon icon="social:share"/>
@@ -37,16 +36,9 @@ const WodDisplay = props => {
 };
 
 WodDisplay.propTypes = {
-  id: React.PropTypes.string,
-  author: React.PropTypes.string,
-  location: React.PropTypes.arrayOf(React.PropTypes.object),
-  date: React.PropTypes.string,
-  description: React.PropTypes.string,
-  likes: React.PropTypes.number,
-  addComment: React.PropTypes.func,
-  addAttendee: React.PropTypes.func,
-  comments: React.PropTypes.arrayOf(React.PropTypes.object),
-  attendees: React.PropTypes.arrayOf(React.PropTypes.object)
+  author: React.PropTypes.string.required,
+  description: React.PropTypes.string.required,
+  images: React.PropTypes.arrayOf(React.PropTypes.object).required
 };
 
 export default WodDisplay;
