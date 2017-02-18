16e52c8ea9b57a4fcd63b946702c2dd40ae5c78e
charles <charleslavong@gmail.com>
2017-02-18 13:59:46 +0100

    Simplying the funcitons to contain just the object expressions which are being returned.

--- src/actions/startupActions.js
+++ src/actions/startupActions.js
@@ -1,10 +1,8 @@
 import * as types from '../constants/actionTypes';
 
-export const addNewAthlete = loginInData => {
-  return {
+export const addNewAthlete = loginInData => ({
     type: types.SAVE_NEW_ATHLETE,
     name: loginInData.name,
     email: loginInData.email
-  };
-};
+});
 
--- src/actions/wodActions.js
+++ src/actions/wodActions.js
@@ -1,47 +1,39 @@
 import * as types from '../constants/actionTypes';
 
-export function saveWod(wod) {
-  return {
-      type: types.ADD_WOD,
-      author: wod.author,
-      date: getDate(),
-      location: wod.location,
-      description: wod.description,
-    };
-}
+export const saveWod = (wod) => ({
+    type: types.ADD_WOD,
+    author: wod.author,
+    date: getDate(),
+    location: wod.location,
+    description: wod.description,
+});
 
-export function addComment(comment) {
-  return {
-    type: types.ADD_COMMENT,
-    id: comment.id,
-    author: comment.author,
-    comment: comment.comment
-  };
-}
+export const addComment = (comment)  => ({
+        type: types.ADD_COMMENT,
+        id: comment.id,
+        author: comment.author,
+        comment: comment.comment
+});
 
-export function addAttendee(attendee) {
-  return {
-    type: types.ADD_ATTENDEE,
-    id: attendee.id,
-    name: attendee.name
-  };
-}
+export const addAttendee = (attendee) => ({
+        type: types.ADD_ATTENDEE,
+        id: attendee.id,
+        name: attendee.name
+});
 
-export function likeWod(wodId) {
-    return {
-      type: types.ADD_LIKE,
-      id: wodId
-    };
-}
+export const likeWod = (wodId) => ({
+        type: types.ADD_LIKE,
+        id: wodId
+});
 
 //only exporting this function in support of unit tests
 export function getDate() {
-  let d = new Date();
-  let days = ['Monday', 'Tuesday', 'Wednesday',
-            'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  let months = ['January', 'February', 'March', 'April', 'May',
-                'June', 'July', 'August', 'September', 'October',
-                'November', 'December'];
+    let d = new Date();
+    let days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let months = ['January', 'February', 'March', 'April', 'May',
+        'June', 'July', 'August', 'September', 'October',
+        'November', 'December'];
 
-  return days[d.getDay()] +" the "+ d.getDate() +" of "+ months[d.getMonth()];
+    return days[d.getDay()] + " the " + d.getDate() + " of " + months[d.getMonth()];
 }
